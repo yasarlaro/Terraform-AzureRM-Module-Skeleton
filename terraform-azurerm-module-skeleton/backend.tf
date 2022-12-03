e99d8eba30a75773d97cd7e538b2c4b566650ff0
yasarlaro <yasarlar_onur@hotmail.com>
2022-12-03 14:56:47 +0100

    removing backend.tf as not needed in the module

--- terraform-azurerm-module-skeleton/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "xxx"
-    storage_account_name = "xxx"
-    container_name       = "tfstate"
-    key                  = "xxx.xxx.tfstate"
-  }
-}
